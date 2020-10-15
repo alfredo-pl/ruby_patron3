b7451ec96ef49f56b00d5470a80c285f65c2bc00
alfredopl <aalfredoplz@gmail.com>
2020-10-15 09:31:32 -0300

    Completed

--- patron3.rb
+++ patron3.rb
@@ -1,2 +1,12 @@
-n = ARGV[0].to_i
+n = ARGV[0].to_i - 1
+n *= 6
 n.times do |i|
+    if i%6 == 0 || i%6 ==1
+        print "."
+    elsif
+        i%6 == 2 || i%6 ==3
+        print "*"
+    else
+        print "|"
+    end
+end
